ab289c358972d02fff72965c9a6c14fc728a4762
Erin Stalzer <erinstalzer@gmail.com>
2019-05-13 05:11:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,13 @@ class TicTacToe
 
   WIN_COMBINATIONS = [
     [@board[0] = "X" && @board[1] = "X" && @board[2] = "X"],
-    []
+    [@board[0] = "X" && @board[1] = "X" && @board[2] = "X"],
+    [@board[0] = "X" && @board[1] = "X" && @board[2] = "X"],
+    [@board[0] = "X" && @board[1] = "X" && @board[2] = "X"],
+    [@board[0] = "X" && @board[1] = "X" && @board[2] = "X"],
+    [@board[0] = "X" && @board[1] = "X" && @board[2] = "X"],
+    [@board[0] = "X" && @board[1] = "X" && @board[2] = "X"],
+    [@board[0] = "X" && @board[1] = "X" && @board[2] = "X"]
   ]
 
   attr_accessor :board
