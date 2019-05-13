198850812044cf9272e35d1d456bed0f11ec2041
Erin Stalzer <erinstalzer@gmail.com>
2019-05-13 05:13:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,16 +2,16 @@ class TicTacToe
 
     attr_accessor :board
 
-  WIN_COMBINATIONS = [
-    [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
-    [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
-    [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
-    [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
-    [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
-    [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
-    [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
-    [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"]
-  ]
+  # WIN_COMBINATIONS = [
+  #   [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
+  #   [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
+  #   [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
+  #   [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
+  #   [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
+  #   [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
+  #   [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"],
+  #   [@board[0] == "X" && @board[1] == "X" && @board[2] == "X"]
+  # ]
 
   def initialize
     @board = [" "," "," "," "," "," "," "," "," "]
